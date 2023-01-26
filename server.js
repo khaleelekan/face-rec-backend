735049e136d5be8e3c2829a537b6cebe48d05b92
unknown <kalilulekan@gmail.com>
2023-01-26 17:56:51 +0100

    knex pg

--- server.js
+++ server.js
@@ -2,6 +2,16 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const bcrypt = require('bcrypt-nodejs')
 const cors = require('cors')
+const knex = require('knex')({
+    client: 'pg',
+    connection: {
+      host : '127.0.0.1',
+      port : 3306,
+      user : 'postgres',
+      password : 'Raheemjr@123',
+      database : 'face-rec'
+    }
+  });
 
 
 const app = express();
